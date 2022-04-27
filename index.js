8335716a0356ae50c9868043d7fc8e1a1b128e57
Tony Stark Jr <st.juniordeveloper@gmail.com>
2022-04-27 22:47:10 +0545

    adding more file

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-// Creating a portfolio tabbed component
+const heroSection = document.querySelector(".section-hero")
 
+
+// Creating a responsive navbar component start
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const headerElem = document.querySelector(".header");
+
+mobile_nav.addEventListener("click", () => {
+    headerElem.classList.toggle("active")
+})
+// Creating a responsive navbar component end
+
+
+// Creating a sticky responsive navbar component start
+const observer = new IntersectionObserver((entries) => {
+    const ent = entries[0];
+    // console.log(ent);
+    !ent.isIntersecting ? document.body.classList.add("sticky") : document.body.classList.remove("sticky")
+}, { root: null, threshold: 0 })
+
+observer.observe(heroSection);
+// Creating a sticky responsive navbar component end
+
+
+
+// Creating a portfolio tabbed component
 
 const p_btns = document.querySelector('.p-btns');
 const p_btn = document.querySelectorAll('.p-btn');
@@ -7,6 +31,8 @@ const p_img_elem = document.querySelectorAll(".img-overlay");
 
 p_btns.addEventListener("click", (e) => {
     const p_btn_clicked = e.target;
+
+ if(! p_btn_clicked.classList.contains('p-btn')) return;
 
     p_btn.forEach((curElem) => {
         curElem.classList.remove("p-btn-active")
@@ -43,8 +69,29 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
+const myJsmedia = (widthSize) => {
+    if (widthSize.matches) {
+      new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      });
+    } else {
+      new Swiper(".mySwiper", {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      });
+    }
+  };
+  
+  const widthSize = window.matchMedia("(max-width: 780px)");
+  // Call listener function at run time
+  myJsmedia(widthSize);
+  // Attach listener function on state changes
+  widthSize.addEventListener("change", myJsmedia);
+  
+
 // Scrooll to top button
-const heroSection = document.querySelector(".section-hero")
+
 const footerElem = document.querySelector(".section-footer")
 
 const scroollElement = document.createElement("div");
@@ -74,3 +121,43 @@ document.querySelector(".hireme-btn").addEventListener('click', (e) => {
     e.preventDefault();
     contactSec.scrollIntoView({ behavior: "smooth" })
 })
+
+
+
+const workSection = document.querySelector('.section-work-data')
+
+const workObserver = new IntersectionObserver((entries, observer) => {
+    const [entry] = entries;
+    console.log(entry);
+    // if (entry.isIntersecting == false) {}
+    if (!entry.isIntersecting) return;
+    // Animate Number Counter
+    const counterNum = document.querySelectorAll(".counter-numbers");
+
+    const speed = 20;
+
+    counterNum.forEach((curElem) => {
+        const updateNumber = () => {
+            const targetNumber = parseInt(curElem.dataset.number)
+            // console.log(targetNumber);
+            const initialNum = parseInt(curElem.innerText);
+            // console.log(initialNum);
+
+            const incrementNumber = Math.trunc(targetNumber / speed);
+            // console.log(incrementNumber);
+
+            if (initialNum < targetNumber) {
+                curElem.innerText = `${initialNum + incrementNumber}+`
+
+                setTimeout(updateNumber, 100)
+            }
+        }
+        updateNumber();
+    });
+
+    observer.unobserve(workSection)
+}, {
+    root: null, treshhold: 0,
+})
+
+workObserver.observe(workSection)
